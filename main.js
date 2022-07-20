e5627558a41e7efa93f0851fb13a5784df8c7189
Iskandar <iskandartojimatov5171@gmail.com>
2022-07-20 10:11:57 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// const number = 8; //number
+// const firstName = "Iskandar"; //number
+// const bool = false; //boolean
+
+// const obj = {
+//   firstName: "Iskandar",
+//   lastName: "Tojimatov",
+//   age: 21,
+//   isMarried: false,
+// };
+
+// console.log(obj.age);
+
+// const arr = ["1", "2", "3", "4", "5"];
+// console.log(arr[2]);
+
+const name = prompt("Ismingizni kiriting:", "");
+
+alert(`Xush kelibsiz ${name}`);
